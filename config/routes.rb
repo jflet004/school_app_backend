da4e383bc7cf96532ff1f06e4ddd9092267e6ff2
Jose Angel Fletes <joseangelfletes@gmail.com>
2025-08-23 00:29:35 -0700

    Add students/export route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :students do
-    resources :parent_contacts, only: [:index, :create, :update, :destroy, :show]
+      collection do
+        get :export   # /students/export(.:format) supports .csv and .xlsx
+      end
+      resources :parent_contacts, only: [:index, :create, :update, :destroy, :show]
   end
 end
