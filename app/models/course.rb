3991806dd9bfde33810a5ab5206ce2a85f8f8ff3
Jose Angel Fletes <joseangelfletes@gmail.com>
2025-08-25 19:35:01 -0700

    add Course and CourseOffering models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ApplicationRecord
-  belongs_to :teacher
-  validates :name, presence: true
+  has_many :course_offerings, dependent: :destroy
+
+  enum :course_type, { group: 0, individual: 1 }, prefix: true
+
+  validates :name, presence: true, uniqueness: true
+  validates :price_cents, numericality: { greater_than_or_equal_to: 0 }
 end
